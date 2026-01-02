d4e249c98f2ac7b1300228a058bcdf9753a160ce
Javed <javed.mistari@webmaffia.com>
2026-01-02 19:05:05 +0530

    other-statutory-information

--- src/app/investors/other-statutory-information/page.js
+++ src/app/investors/other-statutory-information/page.js
@@ -120,7 +120,7 @@ export default async function OtherStatutoryInformationPage() {
       <VotingResults data={votingData} />
       <VotingTable data={votingTableData} />
       <IEPFTable data={iepfTableData} />
-      <NoticeTable data={noticeTableData} />
+
       <SubscriberUpdated data={subscriberData} />
     </div>
   );
